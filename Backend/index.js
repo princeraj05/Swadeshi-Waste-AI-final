d4a8eeb0ea10193b968290768fad227877c49434
Prince Raj <princerajmne@gmail.com>
2026-03-16 16:56:57 +0530

    fix cors for vercel domain

--- Backend/index.js
+++ Backend/index.js
@@ -2,20 +2,23 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+
 const connectDB = require("./config/db");
+
 const testRoute = require("./routes/testRoute");
-// const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const wasteValueRoutes = require("./routes/wasteValueRoutes");
 const contactRoutes = require("./routes/contactRoutes");
+
 const app = express();
 
+/* CORS FIX */
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://swadeshi-waste-gzo367aq9-himanshu-antas-projects.vercel.app",
+      "https://swadeshi-waste-ai-final.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
@@ -29,14 +32,16 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 connectDB();
 
+/* ROUTES */
 app.use("/api/test", testRoute);
-// app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/waste-value", wasteValueRoutes);
 app.use("/api/support", contactRoutes);
 
-const PORT = process.env.PORT;
+/* SERVER */
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(` Backend running on http://localhost:${PORT}`);
+  console.log(`Backend running on port ${PORT}`);
 });
